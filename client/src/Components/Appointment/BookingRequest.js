8432842f88afe6a2ebbc2065cc5b7607ccc533dc
Mudra Verma <md393822@dal.ca>
2023-07-26 21:57:06 -0300

    Consistent Styling

--- client/src/Components/Appointment/BookingRequest.js
+++ client/src/Components/Appointment/BookingRequest.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { makeStyles } from "@mui/styles";
 import {
+  Box,
   Typography,
   TextField,
   Button,
@@ -11,6 +12,7 @@ import {
   MenuItem,
 } from "@mui/material";
 import DatePicker from "react-datepicker";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
@@ -28,12 +30,13 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
     borderRadius: "8px",
     backgroundColor: "#f9f9f9", // Light gray background color
+    width: "80%", // Set the width of the form container to 80%
+    maxWidth: "600px", // Set a maximum width for larger screens
   },
   formHeading: {
     marginBottom: theme.spacing(4),
     color: "#007bff", // Blue color for heading
     textAlign: "center",
-    fontFamily: "Poppins, sans-serif", // New font family
     fontWeight: "bold",
     fontSize: "3rem",
     textTransform: "uppercase",
@@ -46,7 +49,6 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(2),
     color: "#555",
     textAlign: "center",
-    fontFamily: "Poppins, sans-serif", // Same font family for subheading
     fontWeight: "bold",
     fontSize: "1.2rem",
     [theme.breakpoints.down("sm")]: {
@@ -54,7 +56,7 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   formField: {
-    marginBottom: theme.spacing(3),
+    marginBottom: theme.spacing(2),
     width: "100%",
     backgroundColor: "#fff", // White background for text fields
     borderRadius: "4px",
@@ -65,6 +67,9 @@ const useStyles = makeStyles((theme) => ({
       "&:hover fieldset": {
         borderColor: "#999",
       },
+    },
+    "& .MuiOutlinedInput-input": {
+      padding: "10px 12px", // Reduce the height of the text fields
     },
   },
   timeSelect: {
@@ -88,6 +93,22 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "#0056b3", // Darker blue background on hover
     },
   },
+
+  PrimaryButton: {
+    backgroundColor: "#27374d",
+    color: "#ffffff",
+    border: "none",
+    borderRadius: "10px",
+    padding: "12px 24px",
+    fontSize: "16px",
+    cursor: "pointer",
+    display: "inline-block",
+    position: "relative",
+    "&:hover": {
+      opacity: 0.9,
+    },
+    width: "100%",
+  },
   dateAndTime: {
     marginTop: theme.spacing(2),
   },
@@ -99,6 +120,7 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2), // Add some padding for better spacing on mobile
   },
 }));
+
 
 const BookingRequest = () => {
   const classes = useStyles();
@@ -167,82 +189,205 @@ const BookingRequest = () => {
       // Add any logic for error handling (e.g., show error message, etc.)
     }
   };
-
+  
   return (
-    <Card className={classes.bookingForm}>
-      <CardContent>
-        <Typography variant="h2" className={classes.formHeading}>
-          <span role="img" aria-label="Calendar Icon">
-            🗓️
-          </span>{" "}
-          Schedule an Appointment
-        </Typography>
-        <Typography variant="h4" className={classes.formSubHeading}>
-          Choose a date and time for your appointment
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <div className={classes.mobileFormContainer}>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <Card style={{ width: '80%', backgroundColor: '#f5f5f5', color: '#27374d', marginTop: '100px' }}>
+        <CardContent>
+          <Typography variant="h4" className="mb-4 form-heading">
+            <span role="img" aria-label="Calendar Icon">
+              🗓️
+            </span>{" "}
+            Schedule an Appointment
+          </Typography>
+          <Typography variant="h6" className="mb-4 form-subheading">
+            Choose a date and time for your appointment
+          </Typography>
+          <form onSubmit={handleSubmit}>
+          {/* <div style={{ marginBottom: '20px' }}>
+          <Box mb={2}>
             <TextField
               label="Name"
-              variant="outlined"
-              className={classes.formField}
+              variant="standard"
+              fullWidth
               required
             />
+          </Box>
+          <Box mb={2}>
             <TextField
               label="Email"
-              variant="outlined"
-              className={classes.formField}
+              variant="standard"
+              fullWidth
               required
               name="email" // Add a name attribute to retrieve the email in the handleSubmit function
               error={emailError}
               helperText={emailError ? "Invalid email format" : ""}
             />
-          </div>
-          <Grid container spacing={2} className={classes.dateAndTime}>
-            <Grid item xs={12} sm={6}>
-              <DatePicker
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                className={classes.formField}
-                placeholderText="Select date"
-                dateFormat="MM/dd/yyyy"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Select
-                value={selectedTime}
-                onChange={(e) => setSelectedTime(e.target.value)}
-                displayEmpty
-                className={classes.timeSelect}
-                error={timeError}
-              >
-                <MenuItem value="" disabled>
-                  Select time
-                </MenuItem>
-                {availableTimes.map((time) => (
-                  <MenuItem key={time} value={time}>
-                    {time}
+          </Box>
+          {/* Other text fields */}
+          {/* </div> */} 
+          <div style={{ marginBottom: '20px', width: '100%' }}>
+            <input
+              type="text"
+              placeholder="Name*"
+              required
+              className="form-control"
+              style={{
+                backgroundColor: "transparent",
+                borderRadius: "6px",
+                width: "100%",
+                paddingRight: "30px",
+                height: "50px", // Set the desired height for the input field
+                border: "1px solid #ccc", // Add a border to the input field
+                paddingLeft: "12px", // Add some left padding for better appearance
+              }}
+            />
+            </div>
+            <div style={{ marginBottom: '20px', width: '100%' }}>
+
+            <input
+              type="text"
+              placeholder="Email*"
+              required
+              name="email"
+              className="form-control"
+              style={{
+                backgroundColor: "transparent",
+                borderRadius: "6px",
+                width: "100%",
+                paddingRight: "30px",
+                height: "50px", // Set the desired height for the input field
+                border: "1px solid #ccc", // Add a border to the input field
+                paddingLeft: "12px", // Add some left padding for better appearance
+              }}
+            />
+
+            </div>
+            <Grid container spacing={2} style={{ marginBottom: '20px' }}>
+              <Grid item xs={12} sm={6} md={6}>
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={(date) => setSelectedDate(date)}
+                  className="form-control form-field"
+                  placeholderText="Select date"
+                  dateFormat="MM/dd/yyyy"
+                  customInput={<TextField fullWidth variant="outlined" />}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={6}>
+                <Select
+                  value={selectedTime}
+                  onChange={(e) => setSelectedTime(e.target.value)}
+                  displayEmpty
+                  fullWidth
+                  variant="outlined"
+                  error={timeError}
+                  style={{ width: '40%' }}
+                >
+                  <MenuItem value="" disabled>
+                    Select time
                   </MenuItem>
-                ))}
-              </Select>
-              {timeError && (
-                <Typography variant="caption" color="error">
-                  Invalid time format (HH:00)
-                </Typography>
-              )}
+                  {availableTimes.map((time) => (
+                    <MenuItem key={time} value={time}>
+                      {time}
+                    </MenuItem>
+                  ))}
+                </Select>
+                {timeError && (
+                  <Typography variant="caption" color="error">
+                    Invalid time format (HH:00)
+                  </Typography>
+                )}
+              </Grid>
             </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            variant="contained"
-            className={classes.submitButton}
-          >
-            Book Appointment
-          </Button>
-        </form>
-      </CardContent>
-    </Card>
+            <button className={classes.PrimaryButton}>
+              Book Appointment
+              </button>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
 export default BookingRequest;
+  
+
+//   return (
+//     <Card className={classes.bookingForm}>
+//       <CardContent>
+//         <Typography variant="h2" className={classes.formHeading}>
+//           <span role="img" aria-label="Calendar Icon">
+//             🗓️
+//           </span>{" "}
+//           Schedule an Appointment
+//         </Typography>
+//         <Typography variant="h4" className={classes.formSubHeading}>
+//           Choose a date and time for your appointment
+//         </Typography>
+//         <form onSubmit={handleSubmit}>
+//           <div className={classes.mobileFormContainer}>
+//             <TextField
+//               label="Name"
+//               variant="outlined"
+//               className={classes.formField}
+//               required
+//             />
+//             <TextField
+//               label="Email"
+//               variant="outlined"
+//               className={classes.formField}
+//               required
+//               name="email" // Add a name attribute to retrieve the email in the handleSubmit function
+//               error={emailError}
+//               helperText={emailError ? "Invalid email format" : ""}
+//             />
+//           </div>
+//           <Grid container spacing={2} className={classes.dateAndTime}>
+//             <Grid item xs={12} sm={6}>
+//               <DatePicker
+//                 selected={selectedDate}
+//                 onChange={(date) => setSelectedDate(date)}
+//                 className={classes.formField}
+//                 placeholderText="Select date"
+//                 dateFormat="MM/dd/yyyy"
+//               />
+//             </Grid>
+//             <Grid item xs={12} sm={6}>
+//               <Select
+//                 value={selectedTime}
+//                 onChange={(e) => setSelectedTime(e.target.value)}
+//                 displayEmpty
+//                 className={classes.timeSelect}
+//                 error={timeError}
+//               >
+//                 <MenuItem value="" disabled>
+//                   Select time
+//                 </MenuItem>
+//                 {availableTimes.map((time) => (
+//                   <MenuItem key={time} value={time}>
+//                     {time}
+//                   </MenuItem>
+//                 ))}
+//               </Select>
+//               {timeError && (
+//                 <Typography variant="caption" color="error">
+//                   Invalid time format (HH:00)
+//                 </Typography>
+//               )}
+//             </Grid>
+//           </Grid>
+//           <Button
+//             type="submit"
+//             variant="contained"
+//             className={classes.submitButton}
+//           >
+//             Book Appointment
+//           </Button>
+//         </form>
+//       </CardContent>
+//     </Card>
+//   );
+// };
+
+// export default BookingRequest;
